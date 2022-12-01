ae6b64eb41ba6d41264575456783aebafcc8444f
Mostafa Samir <mostafa.3210@gmail>
2022-12-01 18:01:33 +0200

    handle unintialized conda environment

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 conda create -n ai-academy-env python=3.8
-conda deactivate && \
-    conda activate ai-academy-env
-    pip install --upgrade pip
-    pip install -r requirements.txt
+
+source $(conda info --base)/etc/profile.d/conda.sh && \
+    conda deactivate && \
+    conda activate ai-academy-env && \
+    pip install --upgrade pip && \
+    pip install -r requirements.txt && \
+    python -m ipykernel install --user --display-name="AI-Academy"
