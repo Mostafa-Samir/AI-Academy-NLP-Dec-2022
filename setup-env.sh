801f7ba6a53329c39ceda133603d784e68d8df36
Mostafa Samir <mostafa.3210@gmail>
2022-12-17 13:21:17 +0400

    include nltk and its stopwords corpus in setup script

--- setup-env.sh
+++ setup-env.sh
@@ -7,4 +7,5 @@ source $(conda info --base)/etc/profile.d/conda.sh && \
     conda activate ai-academy-env && \
     pip install --upgrade pip && \
     pip install -r requirements.txt && \
-    python -m ipykernel install --user --display-name="AI-Academy"
+    python -m ipykernel install --user --display-name="AI-Academy" && \
+    python -m setup-nltk
